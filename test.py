5ba7d28fc534ba9ee9116f15fcd661a85ab9bc58
Yancale <idantalor100@gmail.com>
2018-09-13 10:54:17 -0700

    added firstly version of parsing the data

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 from googleAPI import *
 from googlemaps import client 
+from google_places import *
 
-location = "London"
-query_temp = "resturants in london"
-radius = "1000"
-client_temp = client.Client(key="AIzaSyAtVGYjp7gGHRu0YcoRAxyB_IruztcMx1s")
-places_result = places(client = client_temp, query = query_temp, radius = radius, open_now = True)
-print(places_result["results"]) 
+location = "london"
+#query_temp = "resturants in london"
+#radius = "1000"
+#client_temp = client.Client(key="AIzaSyAtVGYjp7gGHRu0YcoRAxyB_IruztcMx1s")
+#places_result = places(client = client_temp, query = query_temp, radius = radius, open_now = True)
+#print(places_result) 
+
+google_places = google_places()
+print(google_places.get_initial_places(dates = "10-11.9", place = location))
